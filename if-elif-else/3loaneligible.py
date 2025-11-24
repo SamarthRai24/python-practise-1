55a0542ba54dd929f629404ef14c6ea4c0d58e20
Samarth Rai <samarthrai2624@gmail.com>
2025-11-24 15:39:55 +0530

    Add 3loaneligible.py

--- if-elif-else/3loaneligible.py
+++ if-elif-else/3loaneligible.py
@@ -3,10 +3,10 @@ income = int(input("Annual Income : $"))
 loan = int(input("Exisiting Loans : "))
 
 if (750 < credit and 50000 < income):
-    print ("Eligible")
+    print ("Eligible.")
 elif (650 > credit and 2 < loan):
-    print ("Not Eligible")
+    print ("Not Eligible.")
 elif (750 > credit > 650 and 50000 > income > 30000):
-    print ("Review Required")
+    print ("Review Required.")
 else :
-    print ("Not Eligible")
+    print ("Not Eligible.")
