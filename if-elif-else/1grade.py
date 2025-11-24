7fbb3ab6a88bfaa33397959ef701317112ca1e42
Samarth Rai <samarthrai2624@gmail.com>
2025-11-24 13:24:23 +0530

    Add 1grade.py

---
+++ if-elif-else/1grade.py
@@ -0,0 +1,10 @@
+marks = int(input("Marks obtained : "))
+
+if (marks >= 90):
+    print ("Grade A")
+elif (90 > marks >= 80):
+    print ("Grade B")
+elif (80 > marks >= 70):
+    print ("Grade C")
+else:
+    print ("Grade D")
