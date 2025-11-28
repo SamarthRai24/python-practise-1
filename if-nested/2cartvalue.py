420f78836db8ade95cb1ee502e8ecadf40773ad0
Samarth Rai <samarthrai2624@gmail.com>
2025-11-28 20:59:09 +0530

    Add 2cartvalue.py

---
+++ if-nested/2cartvalue.py
@@ -0,0 +1,18 @@
+c = int(input("Cart Total : ")) #c=Cart Total
+l = input("Loyalty Status Gold/Silver/Bronze : ")
+
+if(c >= 5000):
+    print ("Premium Order")
+    if(l == "Gold" or "gold" or "GOLD"):
+        print (c * 0.8)
+        print ("Shipping Free")
+    elif(l == "Silver" or "silver" or "SILVER"):
+        if(c >= 10000):
+            print (c * 0.85)
+            print ("Shipping Free")
+    else:
+        print (c * 0.9)
+        print ("Shipping Free")
+else:
+    print ("Standard Order")
+    print ("Shipping Charges 200")
